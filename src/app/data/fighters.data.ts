5b4e91100ee9aa03e665e88681abc03a1b4e4ed0
FerSerSan <156015638+FerranSerSan@users.noreply.github.com>
2026-07-22 14:45:17 +0200

    Prepare project for GitHub deployment and polish UI

--- src/app/data/fighters.data.ts
+++ src/app/data/fighters.data.ts
@@ -8,7 +8,7 @@ export const FIGHTERS: Fighter[] = [
     age: 20,
     height: 1,
     weight: 1,
-    image: '/assets/fighters/dani.jpg'
+    image: '/assets/fighters/dani.png'
   },
   {
     id: 'f2',
@@ -17,7 +17,7 @@ export const FIGHTERS: Fighter[] = [
     age: 19,
     height: 1.67,
     weight: 62,
-    image: '/fighters/arnau.jpg'
+    image: '/assets/fighters/arnau.png'
   },
   {
     id: 'f3',
@@ -26,7 +26,7 @@ export const FIGHTERS: Fighter[] = [
     age: 17,
     height: 1.80,
     weight: 70,
-    image: '/fighters/diego.jpg',
+    image: '/assets/fighters/diego.png',
     champion: true
   },
   {
@@ -36,7 +36,7 @@ export const FIGHTERS: Fighter[] = [
     age: 15,
     height: 1.89,
     weight: 79,
-    image: '/fighters/aitor.jpg'
+    image: '/assets/fighters/aitor.png'
   },
   {
     id: 'f5',
@@ -45,6 +45,6 @@ export const FIGHTERS: Fighter[] = [
     age: 18,
     height: 1.82,
     weight: 77,
-    image: '/fighters/jose.jpg'
+    image: '/assets/fighters/jose.png'
   }
 ];
